b09f1003558eabfe55b33f96c5a36a6056a3ae72
Denny <abalone0204@gmail.com>
2015-11-07 17:19:31 +0800

    upgrade react to 14.0.2

--- generators/app/templates/_main.js
+++ generators/app/templates/_main.js
@@ -1,4 +1,7 @@
 require('../static/stylesheets/style.scss');
 import React from 'react';
+import {render} from 'react-dom';
 import Hello from './components/Hello';
-React.render(<Hello />, document.getElementsByClassName('container')[0]);
+render(<Hello />, 
+        document.getElementsByClassName('container')[0]
+        );

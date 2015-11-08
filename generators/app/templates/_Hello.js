de2468027e0102b560f4d8279a58039ebcb293d5
Denny <abalone0204@gmail.com>
2015-11-09 00:17:55 +0800

    feat(): nonsense error

--- generators/app/templates/_Hello.js
+++ generators/app/templates/_Hello.js
@@ -5,9 +5,6 @@ export default class Hello {
     return (
         <div className="greeting">
           <h1>Suku</h1>
-          <p>
-            sudoDennyKu
-          </p>
         </div>
     );
   }
